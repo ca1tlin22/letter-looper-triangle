a1e39a43a863a6df4909edc36b266c3a0bf7983b
ca1tlin22 <gagn0372@algonquinlive.com>
2016-02-23 18:37:40 -0500

    info from previous assignment added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var letter = prompt('Enter a letter:');
+
+var counter;
+
+for (counter = 0; /* start value */ counter < 10; /* when counter ends*/ counter ++) {
+  document.write(letter + '<br>');
+}
